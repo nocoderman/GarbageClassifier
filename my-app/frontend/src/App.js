fa153b8015e76e1751821c9d620fb49f48f150a7
shuu26 <sugarybombs@gmail.com>
2023-03-26 02:53:55 -0400

    test

--- my-app/frontend/src/App.js
+++ my-app/frontend/src/App.js
@@ -1,23 +1,39 @@
+import { useState } from 'react'
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [classifyData, setClassifyData] = useState(null)
+
+  function getData(){
+    axios({
+      method: "GET",
+      url: "/main",
+    })
+    .then((response) => {
+      const res = response.data
+      setClassifyData(({
+        image_prediction: res.name,
+        accuracy_percentage: res.about}))
+    }).catch((error) => {
+      if (error.response){
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      }
+  })}
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        {classifyData && <div>
+              <p>Profile name: {classifyData.profile_name}</p>
+              <p>About me: {classifyData.about_me}</p>
+            </div>
+        }      
     </div>
   );
 }
