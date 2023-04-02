8f35b82f759eda11aba18269247f0647949da341
shuu26 <sugarybombs@gmail.com>
2023-04-02 02:46:41 -0400

    made front end changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
-import React, { useState }  from 'react';  
+import React, { useState, useRef }  from 'react';  
+import ParticlesBg from 'particles-bg';
 import axios from "axios";
 import './App.css';
+import {FaCloudUploadAlt} from 'react-icons/fa'
 
 function App() {
 
   const [classifyData, setClassifyData] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
+
+  const fileInput = useRef(null);
+
 
   const handleFileInput = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -17,6 +22,7 @@ function App() {
       uploadImage(selectedFile);
     }
   };
+
 
   function uploadImage(inputFile) {
     axios({
@@ -50,18 +56,26 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={handleFormSubmit}>
-        <input type="file" onChange={handleFileInput} />
-        <button type="submit">Upload</button>
-      </form>
-      {selectedFile && <img className="temp-img" src={URL.createObjectURL(selectedFile)} alt="Selected Image" />}
-      {(classifyData) && <div>
-      <p>Prediction: {classifyData.image_prediction}</p>
-      </div>}  
+      <ParticlesBg type="cobweb" bg={true} />
+      <div className="main-container">
+        <div className="app-header">
+          <p className="header-text">
+            GARBAGE CLASSIFIER
+          </p>
+        </div>
+        <form onSubmit={handleFormSubmit}>
+          <label className="file-input-label">
+            <input className="file-input" type="file" ref={fileInput} onChange={handleFileInput} style={{display:'none'}}/>
+            <FaCloudUploadAlt/> Attach
+          </label>
+          <button type="submit">Upload</button>
+        </form>
+        {selectedFile && <img className="temp-img" src={URL.createObjectURL(selectedFile)} alt="Selected Image" />}
+        {(classifyData) && <div>
+        <p>Prediction: {classifyData.image_prediction}</p>
+        </div>}  
+      </div>
     </div>
-
-
-
   );
 }
 
